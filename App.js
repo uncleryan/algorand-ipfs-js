0e5e0aa320c8639c38813b7c75c19c7dec174ed2
Khanh Nguyen <khanh@live.com.au>
2023-04-27 19:33:48 +0930

    change ipfs to use localhost

--- App.js
+++ App.js
@@ -1,107 +1,123 @@
-require('dotenv').config()
-const AlgoIPFS = require('./src/AlgoIPFS');
-const main = require('algosdk/src/main');
-const ArgumentParser = require('argparse').ArgumentParser;
+require("dotenv").config();
+const algosdk = require("algosdk");
+const IPFSWrapper = require("./src/IPFSWrapper");
+const AlgoIPFS = require("./src/AlgoIPFS");
+const main = require("algosdk/src/main");
+const ArgumentParser = require("argparse").ArgumentParser;
 
 const ALGOD_CONFIG = {
   algodToken: {
-    'X-API-Key': process.env.API_KEY || ''
+    "X-API-Key": process.env.API_KEY || "",
   },
-  algodServer: process.env.ALGO_SERVER || '',
-  indexerServer: process.env.INDEX_SERVER || '',
-  algodPort: process.env.ALGO_PORT || '',
+  algodServer: process.env.ALGO_SERVER || "",
+  indexerServer: process.env.INDEX_SERVER || "",
+  algodPort: process.env.ALGO_PORT || "",
   account: {
     addr: process.env.ADDRESS,
-    sk: new Uint8Array(process.env.SK.split(','))
-  }
-}
+    sk: new Uint8Array(process.env.SK.split(",")),
+  },
+};
 
 const parseArgs = () => {
   let parser = new ArgumentParser({
-    prog: 'PROG',
+    prog: "PROG",
     add_help: true,
-    description: 'Algorand-IPFS for secure file sharing'
-  })
-  parser.add_argument(
-    '-e', '--example',
-    {
-      action: 'store_true',
-      help: 'Test the complete flow -- Upload to Algorand/IPFS the Algorand white paper and download it shortly after',
-    }
-  )
-  parser.add_argument(
-    '-u', '--upload',
-    {
-      help: 'Encrypt and upload file to IPFS and record hash and filename in Algorand'
-    }
-  )
-  parser.add_argument(
-    '-d', '--download',
-    {
-      help: 'Search filehash in Algorand and proceed to download from IPFS then decrypt it'
-    }
-  )
-  return parser
-}
+    description: "Algorand-IPFS for secure file sharing",
+  });
+  parser.add_argument("-e", "--example", {
+    action: "store_true",
+    help: "Test the complete flow -- Upload to Algorand/IPFS the Algorand white paper and download it shortly after",
+  });
+  parser.add_argument("-u", "--upload", {
+    help: "Encrypt and upload file to IPFS and record hash and filename in Algorand",
+  });
+  parser.add_argument("-d", "--download", {
+    help: "Search filehash in Algorand and proceed to download from IPFS then decrypt it",
+  });
+  return parser;
+};
 
 class App {
-  async main () {
-    let parser = parseArgs()
-    let args = parser.parse_args()
+  async main() {
+    let parser = parseArgs();
+    let args = parser.parse_args();
 
     if (args.example) {
-      await this.example()
+      await this.example();
     } else if (args.upload) {
-      await this.run('upload', args.upload)
+      await this.run("upload", args.upload);
     } else if (args.download) {
-      await this.run('download', args.download)
+      await this.run("download", args.download);
     } else {
-      parser.print_help()
+      parser.print_help();
     }
   }
 
-  sleep (ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+  sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  async example () {
-    const filepath = './assets/algorand_white_paper.pdf'
+  async example() {
+    const filepath = "./assets/algorand_white_paper.pdf";
     const algo_ipfs = new AlgoIPFS({
       ...ALGOD_CONFIG,
       encryptionPassword: process.env.ENCRYPTION_PASSWORD,
-    })
+    });
 
-    await algo_ipfs.init()
-    await algo_ipfs.pushFile(filepath)
+    await algo_ipfs.init();
+    await algo_ipfs.pushFile(filepath);
 
-    console.log('Waiting 5 secs for propagation to Indexer')
-    await this.sleep(5000)
+    console.log("Waiting 5 secs for propagation to Indexer");
+    await this.sleep(5000);
 
-    await algo_ipfs.pullFile(filepath)
+    await algo_ipfs.pullFile(filepath);
   }
 
-  async run (action, filepath) {
+  async run(action, filepath) {
     const algo_ipfs = new AlgoIPFS({
       ...ALGOD_CONFIG,
       encryptionPassword: process.env.ENCRYPTION_PASSWORD,
-    })
-    await algo_ipfs.init()
+    });
+    await algo_ipfs.init();
 
-    if (action === 'upload') {
-      await algo_ipfs.pushFile(filepath)
-    } else if (action === 'download') {
-      await algo_ipfs.pullFile(filepath)
+    if (action === "upload") {
+      await algo_ipfs.pushFile(filepath);
+    } else if (action === "download") {
+      await algo_ipfs.pullFile(filepath);
     } else {
-      error_msg = `Invalid action: ${action} ${filename}`
-      throw error_msg
+      error_msg = `Invalid action: ${action} ${filename}`;
+      throw error_msg;
     }
   }
 }
 
 async function globalMain() {
-  app = new App()
-  await app.main()
-  process.exit()
+  console.log("Starts");
+  console.log("");
+
+  app = new App();
+  await app.main();
+
+  // const ipfs = new IPFSWrapper(process.env.ENCRYPTION_PASSWORD);
+  // await ipfs.init();
+
+  // const client = new algosdk.Algodv2(
+  //   ALGOD_CONFIG.algodToken,
+  //   ALGOD_CONFIG.algodServer,
+  //   ALGOD_CONFIG.algodPort
+  // );
+
+  // const generatedAccount = algosdk.generateAccount();
+  // const passphrase = algosdk.secretKeyToMnemonic(generatedAccount.sk);
+
+  // console.log(`My address: ${generatedAccount.addr}`);
+  // console.log(`SK: ${generatedAccount.sk}`);
+  // console.log(`My passphrase: ${passphrase}`);
+
+  console.log("");
+  console.log("End");
+
+  process.exit();
 }
 
-globalMain()
+globalMain();
--- src/IPFSWrapper.js
+++ src/IPFSWrapper.js
@@ -1,79 +1,103 @@
-const last = require('it-last')
-const ipfs = require('ipfs')
-const path = require('path')
-const fs = require('fs')
-const crypto = require('crypto')
+const last = require("it-last");
+//const ipfs = require('ipfs')
+const ipfs = require("ipfs-http-client");
+const path = require("path");
+const fs = require("fs");
+const crypto = require("crypto");
 
-const ALGORITHM = 'aes-256-cbc';
+const ALGORITHM = "aes-256-cbc";
 const IV_LENGTH = 16; // For AES, this is always 16
 
 module.exports = class IPFSWrapper {
   constructor(encryptionPassword = undefined) {
     if (encryptionPassword) {
-      this.encryptionPassword = crypto.createHash('sha256').update(String(encryptionPassword)).digest('base64').substr(0, 32)
+      this.encryptionPassword = crypto
+        .createHash("sha256")
+        .update(String(encryptionPassword))
+        .digest("base64")
+        .substr(0, 32);
     }
   }
 
   async init() {
-    this.node = await ipfs.create()
-    const version = await this.node.version()
-    console.log('IPFS version:', version.version)
+    this.node = ipfs.create(`${process.env.IPFS_GATEWAY}`);
+    //this.node = await ipfs.create();
+    const version = await this.node.version();
+    console.log("IPFS version:", version.version);
   }
 
   async uploadFile(filepath) {
-    let fileContents = fs.readFileSync(filepath)
+    let fileContents = fs.readFileSync(filepath);
 
     if (Boolean(this.encryptionPassword)) {
-      console.log('Encrypting file')
-      fileContents = this._encryptBuffer(fileContents)
+      console.log("Encrypting file");
+      fileContents = this._encryptBuffer(fileContents);
     }
 
-    const fileAdded = await last(this.node.add({
+    // // for ipfs package
+    // const fileAdded = await last(
+    //   this.node.add({
+    //     path: path.basename(filepath),
+    //     content: fileContents,
+    //   })
+    // );
+
+    const fileAdded = await this.node.add({
       path: path.basename(filepath),
-      content: fileContents
-    }))
+      content: fileContents,
+    });
 
-    console.log('Added file:', fileAdded.path, fileAdded.cid)
+    console.log("Added file:", fileAdded.path, fileAdded.cid);
 
-    return fileAdded
+    return fileAdded;
   }
 
   async downloadFile({ cid, filename }) {
-    console.log('Looking for contents of hash:', cid)
+    console.log("Looking for contents of hash:", cid);
 
-    const chunks = []
+    const chunks = [];
     for await (const chunk of this.node.cat(cid)) {
-      chunks.push(chunk)
+      chunks.push(chunk);
     }
-    let fileContents = Buffer.concat(chunks)
+    let fileContents = Buffer.concat(chunks);
 
     if (Boolean(this.encryptionPassword)) {
-      console.log('Unencrypting file')
-      fileContents = this._decryptBuffer(fileContents)
+      console.log("Unencrypting file");
+      fileContents = this._decryptBuffer(fileContents);
     }
 
-    console.log('File contents retrieved with buffer length:', fileContents.length)
+    console.log(
+      "File contents retrieved with buffer length:",
+      fileContents.length
+    );
 
-    fs.writeFileSync(`_${filename}`, fileContents)
+    fs.writeFileSync(`_${filename}`, fileContents);
 
-    return fileContents
+    return fileContents;
   }
 
   _encryptBuffer(buffer) {
-    console.log('Running encryption on file before uploading')
-    let iv = crypto.randomBytes(IV_LENGTH)
-    let cipher = crypto.createCipheriv(ALGORITHM, this.encryptionPassword, iv)
-    let encrypted = Buffer.concat([cipher.update(buffer), cipher.final()])
-    return Buffer.from(iv.toString('hex') + ':' + encrypted.toString('hex'))
+    console.log("Running encryption on file before uploading");
+    let iv = crypto.randomBytes(IV_LENGTH);
+    let cipher = crypto.createCipheriv(ALGORITHM, this.encryptionPassword, iv);
+    let encrypted = Buffer.concat([cipher.update(buffer), cipher.final()]);
+    return Buffer.from(iv.toString("hex") + ":" + encrypted.toString("hex"));
   }
 
   _decryptBuffer(buffer) {
-    console.log('Running decryption on downloaded file')
-    let textParts = String(buffer).split(':')
-    let iv = Buffer.from(textParts.shift(), 'hex')
-    let encryptedText = Buffer.from(textParts.join(':'), 'hex')
-    let decipher = crypto.createDecipheriv(ALGORITHM, this.encryptionPassword, iv)
-    let decrypted = Buffer.concat([decipher.update(encryptedText), decipher.final()])
-    return decrypted
+    console.log("Running decryption on downloaded file");
+    let textParts = String(buffer).split(":");
+    let iv = Buffer.from(textParts.shift(), "hex");
+    let encryptedText = Buffer.from(textParts.join(":"), "hex");
+    let decipher = crypto.createDecipheriv(
+      ALGORITHM,
+      this.encryptionPassword,
+      iv
+    );
+    let decrypted = Buffer.concat([
+      decipher.update(encryptedText),
+      decipher.final(),
+    ]);
+    return decrypted;
   }
-}
+};
